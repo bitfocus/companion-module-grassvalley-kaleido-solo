83449c2c685f89be6a87cb33d3d28e4389a298d6
imre Liessens <imre.liessens@telenet.be>
2023-05-29 09:07:02 +0200

    Change code skeleton to new format

--- index.js
+++ index.js
@@ -1,251 +1,214 @@
-var instance_skel = require('../../instance_skel');
-var TelnetSocket = require('../../telnet');
+const { InstanceBase, Regex, runEntrypoint, TelnetHelper } = require('@companion-module/base')
+const UpgradeScripts = require('./upgrades')
+
 var actions = require('./actions');
-var debug;
-var log;
 
-const PORT = 13000;
-
-function instance(system, id, config) {
-	var self = this;
-
-	// super-constructor
-	instance_skel.apply(this, arguments);
-	self.status(1,'Initializing');
-	self.actions(); // export actions
-
-	return self;
-}
-
-instance.prototype.updateConfig = function(config) {
-	var self = this;
-	self.config = config;
-	self.init_tcp();
-};
-
-instance.prototype.incomingData = function(data) {
-	var self = this;
-	debug(data);
-
-	self.status(self.STATUS_OK);
+class KaleidoInstance extends InstanceBase {
 	
-	// Process layouts response
-	if(self.commandQueue[0] == "<getKLayoutList/>") {
-		if(data== "<kLayoutList>") return;
+	constructor(internal) {
+		super(internal)
+	}
+	
+	async init(config) {
+		this.config = config
+
+		this.updateStatus('ok')
+
+		this.updateActions() // export actions
 		
-		rawList = data.trim().split('"');
-		rawList = rawList.filter(ele => (ele.trim() != "" && ele.trim() != "</kLayoutList>"))
+		this.port = 13000;
 		
-		self.presetNames = rawList.map(ele => ({"id" : ele, "label": ele}))
-		debug(self.presetNames);
-		self.actions();
+		this.commandQueue = [];
+		this.presetNames = [];
+		this.init_tcp();
 	}
-	
-	// Process end of responses
-	if(data.includes("/")) {
-		// End of response
-		self.commandQueue.shift();
-		self.processQueue();
-	}
-};
-
-instance.prototype.init = function() {
-	var self = this;
-
-	debug = self.debug;
-	log = self.log;
-	
-	self.commandQueue = [];
-	self.presetNames = [];
-	self.init_tcp();
-};
-
-instance.prototype.init_tcp = function() {
-	var self = this;
-
-	if (self.socket !== undefined) {
-		self.socket.destroy();
-		delete self.socket;
-	}
-
-	if (self.config.host) {
-		self.socket = new TelnetSocket(self.config.host, PORT);
-
-		self.socket.on('status_change', function (status, message) {
-			if (status !== self.STATUS_OK) {
-				self.status(status, message);
-			}
-		});
-
-		self.socket.on('error', function (err) {
-			debug("Network error", err);
-			self.log('error',"Network error: " + err.message);
-		});
-
-		self.socket.on('connect', function () {
-			debug("Connected");
-			
-			// Open session
-			self.queueCommand("<openID>" + self.config.host + "_0_4_0_0</openID>");
-			
-			// Read layout names
-			self.queueCommand("<getKLayoutList/>");
-		});
-
-		self.socket.on('error', function (err) {
-			debug("Network error", err);
-			self.log('error',"Network error: " + err.message);
-		});
-
-		// Process incoming data
-		self.socket.on("data", function(buffer) {
-			var indata = buffer.toString("utf8");
-			self.incomingData(indata);
-		});
-	}
-};
-
-// Return config fields for web config
-instance.prototype.config_fields = function () {
-	var self = this;
-
-	return [
-		{
-			type:    'text',
-			id:      'info',
-			width:   12,
-			label:   'Information',
-			value:   'This will establish a TCP connection to a Kaleido multiviewer'
-		},{
-			type:    'textinput',
-			id:      'host',
-			label:   'IP address of the device',
-			width:   12,
-			default: '127.0.0.1',
-			regex:   self.REGEX_IP
+	// When module gets deleted
+	async destroy() {
+		if (this.socket !== undefined) {
+			this.socket.write("<closeID/>\n");
+			this.socket.destroy();
 		}
-	]
-};
-
-// When module gets deleted
-instance.prototype.destroy = function() {
-	var self = this;
-
-	if (self.socket !== undefined) {
-		self.socket.write("<closeID/>\n");
-		self.socket.destroy();
+		
+		this.log('debug', 'destroy')
 	}
 
-	debug("destroy", self.id);
-};
-
-instance.prototype.actions = function(system) {
-	var self = this;
-	self.setActions(actions.getActions(self.presetNames));
-};
-
-instance.prototype.queueCommand = function(command) {
-	var self = this;
-	
-	self.commandQueue.push(command);
-	debug("Queued : "+command);
-	
-	if (self.commandQueue.length == 1) { // If the new command is the only one
-		debug("-> Immediate send : " + command);
-		// Send right away
-		self.processQueue();
+	async configUpdated(config) {
+		this.config = config
+		this.init_tcp();
 	}
-};
 
-instance.prototype.processQueue = function() {
-	var self = this;
-	
-	if(self.commandQueue.length == 0) {
-		// Currently nothing to send
-		return;
+	incomingData(data) {
+		var self = this;
+		debug(data);
+
+		self.status(self.STATUS_OK);
+		
+		// Process layouts response
+		if(self.commandQueue[0] == "<getKLayoutList/>") {
+			if(data== "<kLayoutList>") return;
+			
+			rawList = data.trim().split('"');
+			rawList = rawList.filter(ele => (ele.trim() != "" && ele.trim() != "</kLayoutList>"))
+			
+			self.presetNames = rawList.map(ele => ({"id" : ele, "label": ele}))
+			debug(self.presetNames);
+			self.actions();
+		}
+		
+		// Process end of responses
+		if(data.includes("/")) {
+			// End of response
+			self.commandQueue.shift();
+			self.processQueue();
+		}
+	};
+
+	init_tcp() {
+		var self = this;
+		if (self.socket !== undefined) {
+			self.socket.destroy();
+			delete self.socket;
+		}
+
+		if (self.config.host) {
+			self.socket = new TelnetHelper(this.config.host, this.port);
+
+			self.socket.on('status_change', function (status, message) {
+				if (this !== self.STATUS_OK) {
+					self.updateStatus(status,message)
+				}
+			});
+
+			self.socket.on('error', function (err) {
+				self.log('error',"Network error: " + err.message);
+			});
+
+			self.socket.on('connect', function () {
+				self.log("Connected");
+				
+				// Open session
+				self.queueCommand("<openID>" + self.config.host + "_0_4_0_0</openID>");
+				
+				// Read layout names
+				self.queueCommand("<getKLayoutList/>");
+			});
+
+			self.socket.on('error', function (err) {
+				self.log('error',"Network error: " + err.message);
+			});
+
+			// Process incoming data
+			self.socket.on("data", function(buffer) {
+				var indata = buffer.toString("utf8");
+				self.incomingData(indata);
+			});
+		}
+	};
+
+	// Return config fields for web config
+	getConfigFields() {
+		var self = this;
+
+		return [
+			{
+				type:    'static-text',
+				id:      'info',
+				width:   12,
+				label:   'Information',
+				value:   'This will establish a TCP connection to a Kaleido multiviewer'
+			},{
+				type:    'textinput',
+				id:      'host',
+				label:   'IP address of the device',
+				width:   12,
+				default: '127.0.0.1',
+				regex:   self.REGEX_IP
+			}
+		]
+	};
+
+	updateActions() {
+		this.setActionDefinitions(actions.getActions(this.presetNames));
+	};
+
+	queueCommand(command) {
+		var self = this;
+		
+		self.commandQueue.push(command);
+		debug("Queued : "+command);
+		
+		if (self.commandQueue.length == 1) { // If the new command is the only one
+			debug("-> Immediate send : " + command);
+			// Send right away
+			self.processQueue();
+		}
+	};
+
+	processQueue() {
+		var self = this;
+		
+		if(self.commandQueue.length == 0) {
+			// Currently nothing to send
+			return;
+		}
+		
+		command = self.commandQueue[0]; // Only remove from queue after response from device
+		debug("Sending: "+command);
+		
+		if (self.socket !== undefined && self.socket.connected) {
+			self.socket.write(command+"\n");
+		} else {
+			debug('Socket not connected :(');
+		}
 	}
-	
-	command = self.commandQueue[0]; // Only remove from queue after response from device
-	debug("Sending: "+command);
-	
-	if (self.socket !== undefined && self.socket.connected) {
-		self.socket.write(command+"\n");
-	} else {
-		debug('Socket not connected :(');
+
+	tallyCommand(action) {
+		var state = "";
+		var id = 0;
+		var cmd = "";
+		
+		switch(action.options.color) {
+			case 'red':
+				id = 1;
+				break;
+			case 'green':
+				id = 2;
+				break;
+		}
+		
+		if(action.options.active) {
+			state = "MINOR";
+		}
+		else {
+			state = "NORMAL";
+		}
+		
+		command = `<setKStatusMessage>set id="${id}" status="${state}"</setKStatusMessage>`;
+		this.queueCommand(command);
+	}
+
+	alarmCommand(action) {
+		state = action.options.state.toUpperCase();
+		command = `<setKStatusMessage>set id="0" status="${state}"</setKStatusMessage>`;
+		this.queueCommand(command);
+	}
+
+	UMDCommand(action) {
+		text = action.options.text;
+		
+		var command;
+		this.parseVariables(action.options.text, (value) => {
+			command = `<setKDynamicText>set address="0" text="${value}"</setKDynamicText>`;
+		})
+		
+		this.queueCommand(command);
+	}
+
+	presetCommand(action) {
+		name = action.options.name;
+		command = `<setKCurrentLayout>set ${name}</setKCurrentLayout>`;
+		this.queueCommand(command);
 	}
 }
 
-instance.prototype.tallyCommand = function(action) {
-	var state = "";
-	var id = 0;
-	var cmd = "";
-	
-	switch(action.options.color) {
-		case 'red':
-			id = 1;
-			break;
-		case 'green':
-			id = 2;
-			break;
-	}
-	
-	if(action.options.active) {
-		state = "MINOR";
-	}
-	else {
-		state = "NORMAL";
-	}
-	
-	return `<setKStatusMessage>set id="${id}" status="${state}"</setKStatusMessage>`
-}
-
-instance.prototype.alarmCommand = function(action) {
-	state = action.options.state.toUpperCase();
-	
-	return `<setKStatusMessage>set id="0" status="${state}"</setKStatusMessage>`
-}
-
-instance.prototype.UMDCommand = function(action) {
-	text = action.options.text;
-	
-	var command;
-	this.parseVariables(action.options.text, (value) => {
-		command = `<setKDynamicText>set address="0" text="${value}"</setKDynamicText>`
-	})
-	
-	return command;
-}
-
-instance.prototype.presetCommand = function(action) {
-	name = action.options.name;
-	return `<setKCurrentLayout>set ${name}</setKCurrentLayout>`
-}
-
-instance.prototype.action = function(action) {
-
-	var self = this;
-	var id = action.action;
-	var command;
-
-	switch (id) {
-		case 'tally':
-			command = this.tallyCommand(action);
-			break;
-		case 'alarm':
-			command = this.alarmCommand(action);
-			break;
-		case 'umd':
-			command = this.UMDCommand(action);
-			break;
-		case 'preset':
-			command = this.presetCommand(action);
-			break;
-	}
-
-	if (command !== undefined) {
-		self.queueCommand(command);
-	}
-};
-
-instance_skel.extendedBy(instance);
-exports = module.exports = instance;
+runEntrypoint(KaleidoInstance, UpgradeScripts)
